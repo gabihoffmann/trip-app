2242fb25cd8794383de25f826bb7a9a23677e8ad
Gabi Hoffmann <hoffmann.agf@gmail.com>
2022-11-07 13:30:44 -0300

    feat: change app routes
    - add locations and locations/location/:id

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import { LocationPage } from "./pages/locations/location";
+import { SearchLocations } from "./pages/locations/search";
 import { NotFound } from "./pages/notfound";
 
 export function AppRoutes() {
@@ -7,7 +9,14 @@ export function AppRoutes() {
 
   return (
     <Routes>
-      <Route path="/home" element={<div>oi</div>} />
+      {/* Location page */}
+      <Route path="/locations">
+        <Route index element={<SearchLocations />} />
+        <Route path="location">
+          <Route index element={<LocationPage />} />
+          <Route path=":id" element={<LocationPage />} />
+        </Route>
+      </Route>
       {/* unknown routes */}
       <Route path="*" element={<NotFound />} />
     </Routes>
