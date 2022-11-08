5fd8d84400ce5942bec8eed418c846403e1d7a1a
Gabi Hoffmann <hoffmann.agf@gmail.com>
2022-11-08 08:08:00 -0300

    feat: add form location layout

--- src/pages/locations/location/index.tsx
+++ src/pages/locations/location/index.tsx
@@ -1,8 +1,56 @@
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
+
 export function LocationPage() {
   // ---------------------------------------------
   // Transformations
   // ---------------------------------------------
   // Render
 
-  return <div>Form page</div>;
+  return (
+    <Container sx={{ mt: "1rem" }}>
+      <Typography variant="h4">GabiTrip</Typography>
+
+      {/* Form */}
+      <Typography variant="h5">Localidade</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TextField label={"Cidade"} fullWidth required />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField label={"Pais"} fullWidth required />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField label={"Nome do hotel"} fullWidth />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField label={"Telefone do hotel"} fullWidth />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField multiline rows={5} label={"O que fazer? "} fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        spacing={2}
+        marginTop={"1rem"}
+        justifyContent={"space-between"}
+        justifyItems={"center"}
+      >
+        <Grid item xs={4} margin={"auto"}>
+          <Button variant="outlined">Cancelar</Button>
+        </Grid>
+        <Grid item xs={4} margin={"auto"}>
+          <Button variant="outlined" color="error">
+            Remover
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button type="submit" variant="contained">
+            Salvar
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
