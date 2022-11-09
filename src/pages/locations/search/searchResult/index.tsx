d2ae6562fd58563e76e7438d0d79ead485129db1
Gabi Hoffmann <hoffmann.agf@gmail.com>
2022-11-09 16:29:14 -0300

    refactor: show search result table

--- src/pages/locations/search/searchResult/index.tsx
+++ src/pages/locations/search/searchResult/index.tsx
@@ -1,3 +1,13 @@
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { DtoTripLocation } from "../../../../services/api/v1/locations/types/dtoTripLocation";
 
 interface SearchResultsProps {
@@ -7,10 +17,46 @@ interface SearchResultsProps {
 export function SearchResults(props: SearchResultsProps) {
   const { locations } = props;
 
+  const navigate = useNavigate();
+
   // ---------------------------------------------
   // Transformations
   // ---------------------------------------------
   // Render
 
-  return <pre>{JSON.stringify(locations, null, 2)}</pre>;
+  return (
+    <TableContainer>
+      <Table aria-label="simple table">
+        {/* head */}
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{ fontWeight: "bold" }} align="left">
+              Cidade
+            </TableCell>
+            <TableCell sx={{ fontWeight: "bold" }} align="left">
+              Pais
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        {/* body */}
+        <TableBody>
+          {locations?.map((location) => (
+            <TableRow key={location.id}>
+              <TableCell>{location.city}</TableCell>
+              <TableCell>{location.country}</TableCell>
+              <TableCell align="right">
+                <Button
+                  variant="text"
+                  onClick={() => navigate(`location/${location.id}`)}
+                >
+                  Editar
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
